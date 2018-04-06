0a3604dac4a146738bcd7d72da85ab8540688b49
moritz <moritz.fickenscher@fau.de>
2018-04-06 13:45:01 +0200

    try fix

--- actstream/runtests/urls.py
+++ actstream/runtests/urls.py
@@ -1,5 +1,5 @@
 import os
-from django.contrib import admin, auth
+from django.contrib import admin
 from django.views.static import serve
 try:
     from django.urls import include, url
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^media/(?P<path>.*)$', serve,
         {'document_root': os.path.join(os.path.dirname(__file__), 'media')}),
-    url(r'auth/', include('auth.urls')),
+    url(r'auth/', include('django.contrib.auth.urls')),
     url(r'testapp/', include('testapp.urls')),
     url(r'', include('actstream.urls')),
 ]
